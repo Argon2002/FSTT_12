0be07dd3eedb3dfe31a92a65285b07c5efb8aace
Hosein <mahrooghimh@gmail.com>
2026-05-27 15:03:27 +0200

    first

---
+++ src/test/java/tests/DriverLifeCycle.java
@@ -0,0 +1,32 @@
+package tests;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverLifeCycle {
+    
+protected WebDriver driver;
+
+    @BeforeEach
+    public void setUp(){
+        // declaration and instantiation of objects/variables
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("http://openchrome-demo.orangehrmlive.com/"); 
+        driver.manage().timeouts().implicitlyWait(java.time.Duration.ofSeconds(5));
+    }
+
+    @AfterEach
+    public void teardown(){
+        // close Chrome
+        driver.close();
+    }
+}
